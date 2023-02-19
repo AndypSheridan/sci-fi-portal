7c8b251fd8f7494be722e9e37917de6fc03194f8
AndypSheridan <andy.p.sheridan@gmail.com>
2023-02-19 19:50:18 +0000

    Change allowed hosts in settings.py

--- sfportal/settings.py
+++ sfportal/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['sfportal.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['sci-fi-portal.herokuapp.com', 'localhost']
 
 
 # Application definition
