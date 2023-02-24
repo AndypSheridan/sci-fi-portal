79eb108b091bf029fdb435ee3dfcf41a0327b499
AndypSheridan <andy.p.sheridan@gmail.com>
2023-02-24 16:59:53 +0000

    Create Comment classs in models.py

--- sfblog/models.py
+++ sfblog/models.py
@@ -18,15 +18,17 @@ class Book(models.Model):
     slug = models.SlugField(max_length=100, unique=True)
     author = models.CharField(max_length=50)
     synopsis = models.TextField(blank=True)
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="book_posts")
-    created_on = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="book_posts")
+    created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
     review_content = models.TextField()
     cover_image = CloudinaryField('image', default='placeholder')
     rating = models.IntegerField(choices=RATING, default=3)
-    sub_genre = models.ForeignKey(SubGenre, on_delete=CASCADE)
+    sub_genre = models.ForeignKey(SubGenre, on_delete=models.CASCADE)
     status = models.IntegerField(choices=STATUS, default=0)
-    likes = models.ManyToManyField(User, related_name='bookpost_like', blank=True)
+    likes = models.ManyToManyField(
+        User, related_name='bookpost_like', blank=True)
 
     class Meta:
         ordering = [-'created_on']
@@ -36,4 +38,19 @@ class Book(models.Model):
 
     def number_of_likes(self):
         return self.likes.count()
-    
+
+
+class Comment(models.Model):
+    book = models.ForeignKey(
+        Book, on_delete=models.CASCADE, related_name="comments")
+    name = models.Charfield(max_length=60)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+    email = models.EmailField()
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
