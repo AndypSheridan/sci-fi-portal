1ff16590f70eac9799c673f62b6955f2fecc47bc
AndypSheridan <andy.p.sheridan@gmail.com>
2023-02-24 18:26:17 +0000

    Add Comment Admin class in admin.py

--- sfblog/admin.py
+++ sfblog/admin.py
@@ -9,4 +9,14 @@ class BookAdmin(SummernoteModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
     list_filter = ('status', 'created_on')
     list_display = ('title', 'created_by', 'status', 'created_on')
+    search_fields = ['title', 'review_content', 'author']
+    search_fields = ['User__username']
     summernote_fields = ('review_content',)
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('approved', 'name', 'content', 'book', 'created_on')
+    list_filter = ('approved', 'created_on')
+    search_fields = ['name', 'content']
+    actions = ['approve_comments']
--- sfblog/models.py
+++ sfblog/models.py
@@ -49,4 +49,4 @@ class Comment(models.Model):
         ordering = ["created_on"]
 
     def __str__(self):
-        return f"Comment {self.body} by {self.name}"
+        return f"Comment {self.content} by {self.name}"
