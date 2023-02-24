a6fde3e79be92b65b2a30377f67aad53aadeff45
AndypSheridan <andy.p.sheridan@gmail.com>
2023-02-24 18:08:44 +0000

    Add title,user date,status list display in admin

--- sfblog/admin.py
+++ sfblog/admin.py
@@ -8,4 +8,5 @@ class BookAdmin(SummernoteModelAdmin):
 
     prepopulated_fields = {'slug': ('title',)}
     list_filter = ('status', 'created_on')
+    list_display = ('title', 'created_by', 'status', 'created_on')
     summernote_fields = ('review_content',)
