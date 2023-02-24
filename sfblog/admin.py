f97e93bfc6e8db95917d2d654cc9c9e2850aa1d1
AndypSheridan <andy.p.sheridan@gmail.com>
2023-02-24 17:59:25 +0000

    Prepopulate slug field in admin.py

--- sfblog/admin.py
+++ sfblog/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import Book, Comment, SubGenre
+from .models import Book, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
 @admin.register(Book)
 class BookAdmin(SummernoteModelAdmin):
 
+    prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('review_content',)
