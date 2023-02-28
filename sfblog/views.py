2e3e76908973049e17deb03c5644503da2c59a1b
AndypSheridan <andy.p.sheridan@gmail.com>
2023-02-28 14:06:09 +0000

    Import generic and Book, Comment in views.py

--- sfblog/views.py
+++ sfblog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Book, Comment
 
-# Create your views here.
+
+class BookList(generic.ListView):
+    """
+    Uses Book model, only shows Book reviews which are published
+    Orders by most recently created, eight items per page
+    """
+    model = Book
+    queryset = Book.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+    paginate_by = 8
