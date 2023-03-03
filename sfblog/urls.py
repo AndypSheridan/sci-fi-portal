dc06b93a721116f20f5b911f521d28957084c5eb
AndypSheridan <andy.p.sheridan@gmail.com>
2023-03-03 17:56:20 +0000

    Add book detail path to urls.py

--- sfblog/urls.py
+++ sfblog/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.index, name='home'),
     path('books/', views.BookList.as_view(), name='books'),
     path('about/', views.about, name='about'),
-    path('authors/', views.authors, name='authors')
+    path('authors/', views.authors, name='authors'),
+    path('<slug:slug>', views.BookDetail.as_view(), name="book_detail"),
 ]
