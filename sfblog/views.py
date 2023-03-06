eab75303dbfe56be3d2644a690a98cc2d555254a
AndypSheridan <andy.p.sheridan@gmail.com>
2023-03-06 16:38:09 +0000

    Edit render method for comment form in views.py

--- sfblog/views.py
+++ sfblog/views.py
@@ -51,7 +51,14 @@ class BookDetail(View):
 
         comment_form = CommentForm(data=request.POST)
 
-        
+        if comment_form.is_valid():
+            comment_form.instance.email = request.user.email
+            comment_form.instance.name = request.user.username
+            comment = comment_form.save(commit=False)
+            comment.book = book
+            comment.save()
+        else:
+            comment_form = CommentForm()
 
         return render(
             request,
@@ -59,7 +66,7 @@ class BookDetail(View):
             {
                 "book": book,
                 "comments": comments,
-                "commented": False,
+                "commented": True,
                 "liked": liked,
                 "comment_form": CommentForm()
             },
