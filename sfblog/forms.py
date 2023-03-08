36651fbed8cb6b21a2ed7c0d92df063977ed6a7e
AndypSheridan <andy.p.sheridan@gmail.com>
2023-03-08 13:08:03 +0000

    Add bootstrap form group class to form in add_book

--- sfblog/forms.py
+++ sfblog/forms.py
@@ -12,5 +12,15 @@ class BookForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = (
-            'title', 'author', 'synopsis', 'review_content', 'cover_image', 'rating', 'sub_genre')
+            'title', 'author', 'synopsis', 'review_content', 'cover_image', 'rating', 'sub_genre', 'created_by')
+
+        widget = {
+            'title': form.TextInput(attrs={'class': 'form-control'}),
+            'author': form.TextInput(attrs={'class': 'form-control'}),
+            'synopsis': form.TextInput(attrs={'class': 'form-control'}),
+            'review_content': form.TextInput(attrs={'class': 'form-control'}),
+            'cover_image': form.TextInput(attrs={'class': 'form-control'}),
+            'sub_genre': form.TextInput(attrs={'class': 'form-control'}),
+            'created_by': form.TextInput(attrs={'class': 'form-control'}),
+        }
         
