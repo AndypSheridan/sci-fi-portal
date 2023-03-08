216ed4c81d728b6d0c178c7071576b056e340bde
AndypSheridan <andy.p.sheridan@gmail.com>
2023-03-08 11:25:00 +0000

    Import reverse from django.urls in models.py

--- sfblog/models.py
+++ sfblog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
+from django.urls import reverse
 
 STATUS = ((0, 'Draft'), (1, 'Published'))
 RATING = ((1, '1'), (2, '2'), (3, '3'), (4, '4'), (5, '5'))
@@ -35,6 +36,9 @@ class Book(models.Model):
 
     def number_of_likes(self):
         return self.likes.count()
+
+    def get_absolute_url(self):
+        return reverse()
 
 
 class Comment(models.Model):
