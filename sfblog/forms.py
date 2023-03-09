33960abdbd27b6cb32750415bb78f9ab1e574908
AndypSheridan <andy.p.sheridan@gmail.com>
2023-03-09 09:00:19 +0000

    Add margins to h3 in add book form

--- sfblog/forms.py
+++ sfblog/forms.py
@@ -18,8 +18,8 @@ class BookForm(forms.ModelForm):
             'title': forms.TextInput(
                 attrs={'class': 'form-control', 'placeholder': 'Book Title', }),
             'author': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Author Name',}),
-            'synopsis': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Please provide a synopsis here',}),
-            'review_content': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Write your review here',}),
+            'synopsis': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'What happens?',}),
+            'review_content': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Tell us what you think!',}),
             'sub_genre': forms.Select(attrs={'class': 'form-control'}),
             'created_by': forms.Select(attrs={'class': 'form-control'}),
         }
