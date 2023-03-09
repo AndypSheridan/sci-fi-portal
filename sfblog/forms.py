8830f5233e05f1ac29f00a394ec959c43f1e389c
AndypSheridan <andy.p.sheridan@gmail.com>
2023-03-09 17:52:21 +0000

    Adjust created by field in BookForm in forms.py

--- sfblog/forms.py
+++ sfblog/forms.py
@@ -18,8 +18,9 @@ class BookForm(forms.ModelForm):
             'title': forms.TextInput(
                 attrs={'class': 'form-control', 'placeholder': 'Book Title', }),
             'author': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Author Name',}),
+            'created_by': forms.TextInput(attrs={'class': 'form-control'}),
             'synopsis': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'What happens?',}),
             'review_content': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Tell us what you think!',}),
             'sub_genre': forms.Select(attrs={'class': 'form-control'}),
-            'created_by': forms.Select(attrs={'class': 'form-control'}),
+            # 'created_by': forms.Select(attrs={'class': 'form-control'}),
         }
