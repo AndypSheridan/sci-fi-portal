c05c822b2ffc774dc6828520fac1c459f1bfb392
AndypSheridan <andy.p.sheridan@gmail.com>
2023-03-09 10:58:36 +0000

    Add delete book filepath in urls.py

--- sfblog/urls.py
+++ sfblog/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('like/<slug:slug>', views.BookLike.as_view(), name="book_like"),
     path('add_book/', views.AddBook.as_view(), name='add_book'),
     path('book_detail/edit_book/<int:pk>', views.EditBook.as_view(), name="edit_book"),
+    path('book_detail/<int:pk>/delete', views.DeleteBook.as_view(), name="delete_book"),
 ]
