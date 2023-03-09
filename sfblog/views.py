9468130f08cb2ef8678624844c3e1cb2a001a3fb
AndypSheridan <andy.p.sheridan@gmail.com>
2023-03-09 10:50:53 +0000

    Import deleteview in views.py

--- sfblog/views.py
+++ sfblog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic, View
-from django.views.generic import CreateView, UpdateView
+from django.views.generic import CreateView, UpdateView, DeleteView
 from django.http import HttpResponseRedirect
 from .models import Book, Comment
 from .forms import BookForm, CommentForm
