62620e8180efa22e2f33f4c66a8bc9c6a73adea3
AndypSheridan <andy.p.sheridan@gmail.com>
2023-03-12 19:12:18 +0000

    Add code for 404 and 500 error pages in views.py

--- sfblog/views.py
+++ sfblog/views.py
@@ -7,6 +7,21 @@ from .forms import BookForm, CommentForm
 from django.urls import reverse_lazy
 from django.contrib import messages
 from django.contrib.messages.views import SuccessMessageMixin
+from django.template import RequestContext
+
+
+def handler404(request, *args, **argv):
+    response = render_to_response(
+        '404.html', {}, context_instance=RequestContext(request))
+    response.status_code = 404
+    return response
+
+
+def handler500(request, *args, **argv):
+    response = render_to_response(
+        '500.html', {}, context_instance=RequestContext(request))
+    response.status_code = 500
+    return response
 
 
 def index(request):
