9dfcf366ecf013dc65da3cc901bc74f2f3a41172
AndypSheridan <andy.p.sheridan@gmail.com>
2023-03-12 16:09:52 +0000

    Add code for email backend in settings.py

--- sfportal/settings.py
+++ sfportal/settings.py
@@ -161,3 +161,11 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = ''
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
