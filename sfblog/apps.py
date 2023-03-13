1b8de9e9dea5a0a54febb59cea7753bdfecf795d
AndypSheridan <andy.p.sheridan@gmail.com>
2023-03-13 17:24:50 +0000

    Add signals to function in apps.py

--- sfblog/apps.py
+++ sfblog/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class SfblogConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'sfblog'
+
+    def ready(self):
+        import sfblog.signals
--- sfblog/signals.py
+++ sfblog/signals.py
@@ -4,4 +4,13 @@ from django.dispatch import receiver
 from .models import UserProfile
 
 
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
 
