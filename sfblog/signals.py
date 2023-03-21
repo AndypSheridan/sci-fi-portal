9457629102781e03f36097501924bab5a98f13d0
AndypSheridan <andy.p.sheridan@gmail.com>
2023-03-21 13:11:16 +0000

    Add comments to signals.py

--- sfblog/signals.py
+++ sfblog/signals.py
@@ -4,12 +4,14 @@ from django.dispatch import receiver
 from .models import UserProfile
 
 
+# Automatically creates a User Profile when a User signs up to the site
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance)
 
 
+# Saves User Profile and extends User
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.userprofile.save()
