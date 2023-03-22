69d9b78d5cf4ae1bd7ab1dbaf483d2dc4809f3c2
AndypSheridan <andy.p.sheridan@gmail.com>
2023-03-22 10:55:23 +0000

    Set debug to false

--- sfportal/settings.py
+++ sfportal/settings.py
@@ -30,7 +30,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['sci-fi-portal.herokuapp.com', 'localhost']
 
@@ -58,7 +58,6 @@ INSTALLED_APPS = [
 
 SITE_ID = 1
 
-# ACCOUNT_FORMS = {'signup': 'sfblog.forms.CustomSignupForm'}
 ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 LOGIN_REDIRECT_URL = '/'
